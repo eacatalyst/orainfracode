cde3b52e8dcaaf4549e2eca8642cde68d196c9bd
Clayson Willis <claysonwillis@gmail.com>
2020-02-01 17:37:25 -0500

    creating network files for resource manager 02/01/2020

--- terraociinfra/mainvcn/roadshowvcn.tf
+++ terraociinfra/mainvcn/roadshowvcn.tf
@@ -1,8 +1,8 @@
 # ---- use variables defined in terraform.tfvars or bash profile file
 variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
+#variable "user_ocid" {}
+#variable "fingerprint" {}
+#variable "private_key_path" {}
 variable "compartment_ocid" {}
 variable "region" {}
 variable "AD" {}
@@ -16,9 +16,9 @@ terraform {
 provider "oci" {
     region = "${var.region}"
     tenancy_ocid = "${var.tenancy_ocid}"
-    user_ocid = "${var.user_ocid}"
-    fingerprint = "${var.fingerprint}"
-    private_key_path = "${var.private_key_path}"
+   #  user_ocid = "${var.user_ocid}"
+   # fingerprint = "${var.fingerprint}"
+   # private_key_path = "${var.private_key_path}"
 }
 
 data "oci_identity_availability_domains" "ADs" {
